aca26c88b719ba11322a8f5d0967d864cf2490fd
Gabriela Lara <laragabriela237@gmail.com>
2023-01-28 18:52:46 +0000

    updated html and css

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggle() {
+    var x = document.getElementById('div1');
+    
+
+    if(x.style.display ===  "none") {
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+}
+
+function toggle2(){
+    var y = document.getElementById('div2');
+
+    if(y.style.display ===  "none") {
+        y.style.display = "block";
+    }
+    else{
+        y.style.display = "none";
+    }
+}
+
+function toggle3(){
+    var z = document.getElementById('div3');
+
+    if(z.style.display ===  "none") {
+        z.style.display = "block";
+    }
+    else{
+        z.style.display = "none";
+    }
+}    
